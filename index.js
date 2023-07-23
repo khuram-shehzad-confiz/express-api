c20d7d208f9fd20f0c1be18f2f9dd49992fee619
khuram-shehzad-confiz <khuram.shehzad@confiz.com>
2023-07-23 13:35:26 +0500

    Json api

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static('public'))
 
+const data=require('./Data.json')
+app.get('/', (req, res) => {
+  res.send(data)
+})
+app.get('/image', (req, res) => {
+    res.sendFile(__dirname + '/public/burger12.jpg');
+  });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
